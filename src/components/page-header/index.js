997842959b5b45523b7a1de16ec6a5fa7dc7bb85
John Gruber <john.gruber@gitbit.org>
2019-06-09 21:51:30 -0400

    updated navbar colors

--- src/components/page-header/index.js
+++ src/components/page-header/index.js
@@ -49,10 +49,6 @@ export class PageHeader extends React.Component {
     return (
       <nav className="navbar" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
-          <Link to="/" className="navbar-item">
-            <img src="/assets/imgs/gitbit-icon-mercury-50x50.png" width="28" height="28" />
-          </Link>
-
           <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
