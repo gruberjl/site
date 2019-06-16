dffc78bcc57abd9f8a1dbf7c100a630164a4bd65
John Gruber <john.gruber@gitbit.org>
2019-06-15 21:28:45 -0400

    added reddit-auto-update

--- work-server/src/index.js
+++ work-server/src/index.js
@@ -1,16 +1,19 @@
 import {setup} from './setup'
 import {engage} from './engage'
 import {followBack} from './follow-back'
+import {redditAutoUpvote} from './reddit-auto-upvote'
 
 const start = async () => {
   await setup()
-  await engage()
-  await followBack()
+  engage()
+  followBack()
+  redditAutoUpvote()
 }
 
 start()
 setInterval(() => engage(), 300000)
 setInterval(() => followBack(), 18000000)
+setInterval(redditAutoUpvote, 3600000)
 
 import * as server from './server'
 server.start()
