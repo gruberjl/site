4a96591d228431ba2f26e7da89397adbb95c48a6
John Gruber <john.gruber@gitbit.org>
2019-06-19 22:08:07 -0400

    added collection class

--- src/lib/store/journals.js
+++ src/lib/store/journals.js
@@ -1,49 +1,14 @@
-import {firestore} from '../firestore'
-import EventEmitter  from 'events'
+import {Collection}  from './collection'
 import shortid from 'shortid'
 import {convertToRaw, EditorState} from 'draft-js'
-import {auth} from './auth'
 
-class Journals extends EventEmitter {
-  constructor() {
-    super()
-    auth.once(auth.events.login, this.onLogin)
+class Journals extends Collection {
+  constructor(collectionName) {
+    super(collectionName)
+    this.events.activePageChanged = 'ACTIVE_PAGE_CHANGED'
   }
 
-  docs = {}
-  isLoaded = false
   activePage = ''
-
-  events = {
-    docsUpdated: 'docsUpdated',
-    activePageChanged: 'ACTIVE_PAGE_CHANGED'
-  }
-
-  onLogin = (user) => {
-    this.collection = firestore.db.collection('root').doc(user.uid).collection('journals')
-    this.listener = this.collection.onSnapshot(this.onDocsChange)
-  }
-
-  onLogoff = () => {
-    this.docs = {}
-    this.isLoaded = false
-    if (this.listener) this.listener()
-    this.listener = undefined
-    this.collection = undefined
-    auth.once(auth.events.login, this.onLogin)
-  }
-
-  onDocsChange = (snapshot) => {
-    snapshot.forEach(doc => {
-      this.docs[doc.id] = doc.data()
-    })
-
-    this.isLoaded = true
-    this.emit(this.events.docsUpdated, this.docs, snapshot.metadata.fromCache)
-
-    if (this.activePage == '' && Object.keys(this.docs).length > 0)
-      this.setActivePage(Object.values(this.docs)[0].id)
-  }
 
   create = () => {
     const doc = {
@@ -55,14 +20,10 @@ class Journals extends EventEmitter {
     return doc
   }
 
-  set(doc) {
-    firestore.set(this.collection, doc)
-  }
-
   setActivePage = (pageId) => {
     this.activePage = pageId
     this.emit(this.events.activePageChanged, pageId)
   }
 }
 
-export const journals = new Journals()
+export const journals = new Journals('journals')
